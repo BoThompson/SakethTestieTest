b0adad3904d10f16fe233f494bd2a5f082095582
Bo Thompson <AimlessZealot@gmail.com>
2023-04-12 18:38:11 -0400

    I added some code. Yaaaay.

--- test.py
+++ test.py
@@ -1,1 +1,4 @@
 print("This is a test of git as a tool!!!")
+print("Aaaand I'm printing more!!!")
+for i in range(1000):
+    print("This is message number " + str(i) + " and you're still reading!!!")
